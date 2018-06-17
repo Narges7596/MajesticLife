6ecd294f437d2c2c57e37b49c445e29f8d5bfdb3
Narges.F <nrgfar@gmail.com>
2018-06-17 13:52:19 +0430

    - Fixed showing faaliats skill in dialog

--- MajesticLife/app/src/main/java/com/farazannajmi/majesticlife/FaaliatPackage/GainedFromFaaliatDialogFragment.java
+++ MajesticLife/app/src/main/java/com/farazannajmi/majesticlife/FaaliatPackage/GainedFromFaaliatDialogFragment.java
@@ -6,6 +6,7 @@ import android.app.DialogFragment;
 import android.arch.lifecycle.LifecycleOwner;
 import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
@@ -16,11 +17,15 @@ import android.support.v4.app.FragmentActivity;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.util.Log;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.animation.DecelerateInterpolator;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.NumberPicker;
 import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -45,6 +50,12 @@ import java.util.List;
 public class GainedFromFaaliatDialogFragment extends DialogFragment
 {
     private static Faaliat ThisFaaliat;
+    private static ArrayList<Skill> thisFaaliatSkills = new ArrayList<>();
+    private static ArrayList<Integer> preSkillProgresses = new ArrayList<>();
+    private static boolean gotAllFaaliatSkills;
+    private static ArrayList<ProgressBar> skillsProgressBars = new ArrayList<>();
+    private static ArrayList<Integer> preSkillsLevels = new ArrayList<>();
+    private static ArrayList<TextView> skillsLevelTv = new ArrayList<>();
 
     private TextView XpLevel_txt;
     private TextView HpLevel_txt;
@@ -52,6 +63,7 @@ public class GainedFromFaaliatDialogFragment extends DialogFragment
     private ProgressBar XpLevel_progBar;
     private ProgressBar HpLevel_progBar;
     private ProgressBar SpLevel_progBar;
+    private LinearLayout parentLayout;
 
     public static GainedFromFaaliatDialogFragment newInstance(int faaliatIndex, int repeatTime)
     {
@@ -72,12 +84,13 @@ public class GainedFromFaaliatDialogFragment extends DialogFragment
         final int faaliatIndex = getArguments().getInt("faaliatIndex");
         final int repeatTime = getArguments().getInt("repeatTime");
         ThisFaaliat = DataHolder.Faaliats.get(faaliatIndex);
+        gotAllFaaliatSkills = false;
 
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
 
         LayoutInflater inflater = getActivity().getLayoutInflater();
 
-        View view = inflater.inflate(R.layout.dialog_gainedfromfaaliat, null);
+        final View view = inflater.inflate(R.layout.dialog_gainedfromfaaliat, null);
 
         //getting UI elements:
         XpLevel_txt = view.findViewById(R.id.gainedD_XpLevel_txt);
@@ -89,6 +102,7 @@ public class GainedFromFaaliatDialogFragment extends DialogFragment
         XpLevel_progBar.setMax(100);
         HpLevel_progBar.setMax(100);
         SpLevel_progBar.setMax(100);
+        parentLayout = view.findViewById(R.id.gainedD_parent);
 
         //setting UI elements values:
         XpLevel_txt.setText(Integer.toString(DataHolder.ThisUser.getXpLevel()));
@@ -98,22 +112,91 @@ public class GainedFromFaaliatDialogFragment extends DialogFragment
         HpLevel_progBar.setProgress(DataHolder.ThisUser.getHP());
         SpLevel_progBar.setProgress(DataHolder.ThisUser.getSP());
 
-
         //getting FaaliatSkills and setting it in UI:
         FaaliatSkillViewModel faaliatSkillViewModel = ViewModelProviders.of(FaaliatsActivity.appCompatActivity).get(FaaliatSkillViewModel.class);
         faaliatSkillViewModel.getSkillsForFaaliat(ThisFaaliat).observe((LifecycleOwner) FaaliatsActivity.context, new Observer<List<FaaliatSkill>>() {
             @Override
             public void onChanged(@Nullable List<FaaliatSkill> skills) {
-                ArrayList<FaaliatSkill> faaliatSkills = (ArrayList) skills;
-
-                for (int i = 0; i < faaliatSkills.size(); i++)
+                if(!gotAllFaaliatSkills)
                 {
-                    Skill skill = DataHolder.Skills.get(faaliatSkills.get(i).getSkill_ID());
-                    int a = faaliatSkills.get(i).getRepetitionCount();
-                    skill.setProgress(skill.getProgress() + (faaliatSkills.get(i).getRepetitionCount() * repeatTime));
+                    ArrayList<FaaliatSkill> faaliatSkills = (ArrayList) skills;
 
-                    SkillViewModel skillViewModel = ViewModelProviders.of(FaaliatsActivity.appCompatActivity).get(SkillViewModel.class);
-                    skillViewModel.update(skill);
+                    for (int i = 0; i < faaliatSkills.size(); i++)
+                    {
+                        Skill skill = DataHolder.Skills.get(faaliatSkills.get(i).getSkill_ID());
+                        preSkillProgresses.add(skill.getProgress());
+                        preSkillsLevels.add(skill.getLevel());
+
+                        int newProgress = skill.getProgress() + (faaliatSkills.get(i).getRepetitionCount() * repeatTime);
+                        if(newProgress >= 100) //skill level upped
+                        {
+                            skill.setLevel(skill.getLevel() + 1);
+                            newProgress = newProgress - 100;
+                        }
+                        else if((newProgress < 0) && (skill.getLevel() == 1))
+                        {
+                            newProgress = 0;
+                        }
+                        skill.setProgress(newProgress);
+
+                        thisFaaliatSkills.add(skill);
+
+                        SkillViewModel skillViewModel = ViewModelProviders.of(FaaliatsActivity.appCompatActivity).get(SkillViewModel.class);
+                        skillViewModel.update(skill);
+
+
+                        //region ---------- adding skills data to UI ----------
+                        int fifty = getActivity().getResources().getDimensionPixelSize(R.dimen.fivezero);
+                        int oneFifty = getActivity().getResources().getDimensionPixelSize(R.dimen.onefivezero);
+
+                        LinearLayout row = new LinearLayout(view.getContext());
+                        row.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+                        row.setOrientation(LinearLayout.HORIZONTAL);
+
+                        TextView skillName_tv = new TextView(view.getContext());
+                        LinearLayout.LayoutParams tv_params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+                        tv_params.setMargins(2, 0, 10, 0);
+                        skillName_tv.setLayoutParams(tv_params);
+                        skillName_tv.setText(skill.getSkill_Name());
+                        //skillName_tv.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+                        skillName_tv.setTextSize(TypedValue.COMPLEX_UNIT_SP, 15);
+                        row.addView(skillName_tv);
+
+                        RelativeLayout relativeLayout = new RelativeLayout(view.getContext());
+                        relativeLayout.setLayoutParams(new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT));
+
+                            ImageView imageView = new ImageView(view.getContext());
+                            RelativeLayout.LayoutParams image_params = new RelativeLayout.LayoutParams(fifty, fifty);
+                            image_params.addRule(RelativeLayout.CENTER_IN_PARENT, RelativeLayout.TRUE);
+                            imageView.setLayoutParams(image_params);
+                            imageView.setImageResource(R.drawable.ic_circle);
+                            relativeLayout.addView(imageView);
+
+                            TextView skillLevel_tv = new TextView(view.getContext());
+                            RelativeLayout.LayoutParams level_params = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+                            level_params.addRule(RelativeLayout.CENTER_IN_PARENT, RelativeLayout.TRUE);
+                            skillLevel_tv.setLayoutParams(level_params);
+                            skillLevel_tv.setText(Integer.toString(skill.getLevel()));
+                            skillLevel_tv.setTextColor(Color.BLACK);
+                            skillLevel_tv.setTextSize(TypedValue.COMPLEX_UNIT_SP, 15);
+                            skillsLevelTv.add(skillLevel_tv);
+                            relativeLayout.addView(skillLevel_tv);
+
+                        row.addView(relativeLayout);
+
+                        ProgressBar progressBar = new ProgressBar(view.getContext(), null, android.R.attr.progressBarStyleHorizontal);
+                        LinearLayout.LayoutParams prog_params = new LinearLayout.LayoutParams(fifty, oneFifty);
+                        prog_params.setMargins(10,0,0,0);
+                        progressBar.setLayoutParams(prog_params);
+                        progressBar.setMax(100);
+                        progressBar.setProgress(preSkillProgresses.get(i));
+                        skillsProgressBars.add(progressBar);
+                        row.addView(progressBar);
+
+                        parentLayout.addView(row);
+                        //endregion ------------------------------
+                    }
+                    gotAllFaaliatSkills = true;
                 }
             }
         });
@@ -210,10 +293,23 @@ public class GainedFromFaaliatDialogFragment extends DialogFragment
                 //endregion
                 //endregion ------------------------------
 
-                //todo showing skill's progress!!
-
+                //adding skill progress:
+                for(int skillCounter = 0; skillCounter < thisFaaliatSkills.size(); skillCounter++)
+                {
+                    Skill s = thisFaaliatSkills.get(skillCounter);
+                    if(s.getLevel() > preSkillsLevels.get(skillCounter)) //if skill level upped
+                    {
+                        skillsLevelTv.get(skillCounter).setText(Integer.toString(s.getLevel()));
+                        skillsLevelTv.get(skillCounter).setTextColor(ContextCompat.getColor(FaaliatsActivity.context, R.color.colorAccent));
+                    }
+                    skillsProgressBars.get(skillCounter).setProgress(s.getProgress());
+                    ObjectAnimator animationskill = ObjectAnimator.ofInt(skillsProgressBars.get(skillCounter), "progress", preSkillProgresses.get(skillCounter), s.getProgress());
+                    animationskill.setDuration(1000); // in milliseconds
+                    animationskill.setInterpolator(new DecelerateInterpolator());
+                    animationskill.start();
+                }
             }
-        }, 500);
+        }, 1000);
 
         // Add action buttons
         builder.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener()
